33efcfbee2d2b7a9b5a1388ee836473821473b03
AbidShazid3 <abidshazid3@gmail.com>
2024-02-14 17:47:03 +0600

    added all function

--- js scripts/alpha-clash.js
+++ js scripts/alpha-clash.js
@@ -1,3 +1,10 @@
 function playButton(){
-    console.log('hello i am playButton');
+    // step 1 hide the home screen. to hide the screen add the classList hidden to the home section
+    const homeScreen = document.getElementById('home-screen');
+    homeScreen.classList.add('hidden');
+    // console.log(homeScreen.classList);
+
+    // show the playground
+    const playGroundSection = document.getElementById('play-ground');
+    playGroundSection.classList.remove('hidden');
 }
