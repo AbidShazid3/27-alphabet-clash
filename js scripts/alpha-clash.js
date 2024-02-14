04e4bcac97377aa64020cb9d701d4a5c93aa9d68
AbidShazid3 <abidshazid3@gmail.com>
2024-02-14 18:11:40 +0600

    added utility

--- js scripts/alpha-clash.js
+++ js scripts/alpha-clash.js
@@ -1,10 +1,15 @@
-function playButton(){
-    // step 1 hide the home screen. to hide the screen add the classList hidden to the home section
-    const homeScreen = document.getElementById('home-screen');
-    homeScreen.classList.add('hidden');
-    // console.log(homeScreen.classList);
+// function playButton(){
+//     // step 1 hide the home screen. to hide the screen add the classList hidden to the home section
 
-    // show the playground
-    const playGroundSection = document.getElementById('play-ground');
-    playGroundSection.classList.remove('hidden');
+//     const homeScreen = document.getElementById('home-screen');
+//     homeScreen.classList.add('hidden');
+//     // console.log(homeScreen.classList);
+//     // show the playground
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+// }
+
+function playButton(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
